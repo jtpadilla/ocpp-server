2fc3cd9c0d955dd0948e49a22f6cbc31f391f802
Tadeo Julián <tjulian@uji.es>
2023-04-26 11:55:51 +0200

    Se ajustan los parametros para el despliege en servidor

--- java/app/simpleserver/src/main/install/systemd/launch.sh
+++ java/app/simpleserver/src/main/install/systemd/launch.sh
@@ -6,4 +6,4 @@ JAVA_OPTIONS=" -Xms256m -Xmx512m -server "
 APP_OPTIONS=" ${WORKDIR}/config/log.properties ${WORKDIR}/config/parameters.toml ${WORKDIR}/config/connector.toml"
 
 cd $WORKDIR
-"${JAVA_HOME}/bin/java" $JAVA_OPTIONS -jar $WORKDIR/engine-launcher_deploy.jar $APP_OPTIONS
+"${JAVA_HOME}/bin/java" $JAVA_OPTIONS -jar $WORKDIR/simpleserver_deploy.jar $APP_OPTIONS
