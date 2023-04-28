45aab1446c2cf375c9d8b03f04ec60f4f56ea083
padilla <padilla@speedycontrol.com>
2023-04-28 14:38:14 +0200

    Refactorizacion

--- java/websocket/examples/src/main/java/websocket/example/ChatServerExample.java
+++ java/websocket/examples/src/main/java/websocket/example/ChatServerExample.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ChatServerSimple {
+public class ChatServerExample {
 
     public static void main(String[] args) throws InterruptedException, IOException {
 
