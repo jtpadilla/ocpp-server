ed6ca2b0962a30467831de7e20948183f43b3429
padilla <padilla@speedycontrol.com>
2023-04-28 14:31:05 +0200

    Refactorizacion

--- java/websocket/examples/src/main/java/websocket/example/ChatServerSimple.java
+++ java/websocket/examples/src/main/java/websocket/example/ChatServerSimple.java
@@ -1,18 +1,11 @@
 package websocket.example;
 
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 
-/**
- * A simple WebSocketServer implementation. Keeps track of a "chatroom".
- */
 public class ChatServerSimple {
 
     public static void main(String[] args) throws InterruptedException, IOException {
